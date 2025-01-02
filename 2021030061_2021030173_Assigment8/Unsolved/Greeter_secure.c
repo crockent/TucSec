f01eb485fd6c0bf55960a5c11c31b2453e7ccd7a
FenrirRhogar <103751641+FenrirRhogar@users.noreply.github.com>
2025-01-02 16:51:01 +0200

    7k8

---
+++ 2021030061_2021030173_Assigment8/Unsolved/Greeter_secure.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+unsigned char Name[1024];
+
+void readString() {
+	char buf[32];
+	gets(Name);
+        memcpy(buf, Name, strlen(Name));
+        printf("Hello %s %s, have a nice day.\n", Name);
+
+   	return;
+}
+
+
+
+int main(void) {
+
+
+	printf("What is your name?\n");
+	readString();
+	exit(0);
+}
